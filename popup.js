160d3a9e42135346bf28c23d88abe70332a696d5
Ігор Шимков <ihossa17@gmail.com>
2021-08-19 20:00:00 +0300

    send auth req

--- popup.js
+++ popup.js
@@ -1,14 +1,13 @@
 getStartBtn.addEventListener("click", async() => {
   const reg = new RegExp(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/);
   const resEmail = reg.exec(email.value);
-  if(!resEmail){errorEmail.style.visibility = 'visible'}
+  if(!resEmail || email.value.length === 0){errorEmail.style.visibility = 'visible'}
 
   const resPass = password.value.length > 5
   if(!resPass){errorPassword.style.visibility = 'visible'}
 
   if(resEmail && resPass){
   
-    var data = new FormData();
     fetch('http://localhost:3000/v1/auth/signIn', {
       method: 'POST',
       headers: {
@@ -16,13 +15,18 @@ getStartBtn.addEventListener("click", async() => {
       },
       body: JSON.stringify({
         // your expected POST request payload goes here
-        email: "ihor.shymkov@gmail.com",
-        password: "Admin123Ihor"
-          })
+        email: email.value,
+        password: password.value
+      })
     })
     .then((res) => res.json())
     .then((data) => {
-      console.log(data)
+      if(data.hasOwnProperty('token')){
+        console.log(data)
+      }else{
+        resError.style.display = 'flex'
+      }
+     
     })
 
     // chrome.tabs.create({
@@ -33,9 +37,11 @@ getStartBtn.addEventListener("click", async() => {
 
 email.addEventListener("input", () => {
   errorEmail.style.visibility = 'hidden'
+  resError.style.display = "none"
 })
 password.addEventListener("input", () => {
   errorPassword.style.visibility = 'hidden'
+  resError.style.display = "none"
 })
 
 
